61c55bea48f0173fab80c804ca501d45406a0549
BRENO HENRIQUE DE CASTRO ROCHA <breno.hcrocha@senacsp.edu.br>
2023-05-08 17:08:37 -0300

    first commit

---
+++ Fundamentos/VariaveiseConstantes.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CursoCSharp.Fundamentos {
+    class VariaveiseConstantes {
+        public static void Executar() {
+
+            double raio = 1.3;
+
+            const double pi = 3.14;
+
+            double area = pi * raio * raio;
+
+            Console.WriteLine($"A área total é de: {area}");
+
+            string nome = "Breno";
+            int idade = 17;
+            double altura = 1.90;
+            bool temGato = true;
+            var sobrenome = "perez";
+        }
+    }
+    }
---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+using CursoCSharp.Fundamentos;
+
+namespace CursoCSharp {
+    class Program {
+        static void Main(string[] args) {
+            var central = new CentralDeExercicios(new Dictionary<string, Action>() {
+                {"Primeiro Programa - Fundamentos", PrimeiroPrograma.Executar},
+                {"Variasveis e Constantes - Fundamentos", VariaveiseConstantes.Executar },
+                { "Notação Ponto - Fundamentos", NotacaoPonto .Executar },
+            });
+
+            central.SelecionarEExecutar();
+
+
+        }
+
+     
+
+
+
+    }
+}
